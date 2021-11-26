ed1e2596709e2bd60f6a0c8dedee10ef8825941b
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-26 07:31:44 -0500

    dashboard page renders asset components  and trading component

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-// import { Link } from 'react-router-dom';
-
+import AssetContainerComponent from '../components/DashboardComponents/AssetContainer/AssetContainerComponent';
 interface Props {}
 
 const Dashboard: React.FC<Props> = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dashboard</h1>
+      <header className="App-header" style={{ marginLeft: '80px' }}>
+        <AssetContainerComponent />
       </header>
     </div>
   );
