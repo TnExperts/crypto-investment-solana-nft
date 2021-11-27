635b4a82d004723eaa1cc183f22950e99b5143fe
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-26 21:02:51 -0500

    adding firebase config

---
+++ frontend/src/config/config.ts
@@ -0,0 +1,12 @@
+const config = {
+  firebaseConfig: {
+    apiKey: 'AIzaSyAXIBXbIRokQnx6ApEpMlmqK56DyHlSjG0',
+    authDomain: 'crypto-tra-491d2.firebaseapp.com',
+    projectId: 'crypto-tra-491d2',
+    storageBucket: 'crypto-tra-491d2.appspot.com',
+    messagingSenderId: '443334768190',
+    appId: '1:443334768190:web:4fbd77b3902acfe336036f',
+  },
+};
+
+export default config;
