2e4063360cd47a25de1528e28573010f1c67c123
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-26 22:52:42 -0500

    Logging works clean now. TODO: protect dashboard route

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,13 +1,94 @@
 import React from 'react';
-import LoginComponent from '../components/Login/LoginComponent';
+import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../config/firebase';
+import Alert from '@mui/material/Alert';
+import AlertTitle from '@mui/material/AlertTitle';
 
 interface Props {}
 
 const Login: React.FC<Props> = () => {
+  const [isLoggingIn, setIsLoggingIn] = React.useState<boolean>(false);
+  const [userEmail, setUserEmail] = React.useState<string>('');
+  const [userPassword, setUserPassword] = React.useState<string>('');
+  const [error, setError] = React.useState<string>('');
+
+  const history = useHistory();
+
+  const logUserWithEmailAndPassword = () => {
+    if (error !== '') {
+      setError('');
+    }
+    setIsLoggingIn(true);
+    signInWithEmailAndPassword(auth, userEmail, userPassword)
+      .then((userCredential) => {
+        // Signed in
+        // const user = userCredential.user;
+        history.push('/dashboard');
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoggingIn(false);
+        const errorCode = error.code;
+        // const errorMessage = error.message;
+        console.log(errorCode);
+        if (errorCode === 'auth/weak-password') {
+          setError('Password should be at least 6 characters.');
+        } else if (errorCode === 'auth/email-already-in-use') {
+          setError('The email is already in use.');
+        } else if (errorCode === 'auth/invalid-email') {
+          setError('The email is invalid.');
+        }
+      });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <LoginComponent />
+        {error !== '' && (
+          <Alert severity="error">
+            <AlertTitle>{error}</AlertTitle>
+          </Alert>
+        )}
+        <h1>Login</h1>
+        <Box
+          sx={{
+            width: 300,
+            height: 300,
+            borderRadius: '5%',
+          }}
+        >
+          <div>
+            <input
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={(event) => setUserEmail(event.target.value)}
+              value={userEmail}
+            />
+            <input
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={(event) => setUserPassword(event.target.value)}
+              value={userPassword}
+            />
+            <p className="form_extra_font">
+              <Link to="/register" className="link">
+                Don't have an account?
+              </Link>
+            </p>
+            <button
+              type="submit"
+              disabled={isLoggingIn}
+              onClick={logUserWithEmailAndPassword}
+            >
+              Register
+            </button>
+          </div>
+        </Box>
       </header>
     </div>
   );
