886d75727d540d647c798ebf008f75b05a7141f8
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-29 04:21:28 -0500

    setup mock json to test with frontend

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,31 @@
 import express, { Request, Response, NextFunction, Application } from 'express';
 
+const cors = require('cors');
 const app: Application = express();
+
+app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
+});
+
+app.get('/dashboard', (req: Request, res: Response) => {
+  res.json({
+    user: [
+      {
+        name: 'bob',
+        age: 20,
+      },
+      {
+        name: 'alice',
+        age: 21,
+      },
+      {
+        name: 'tom',
+        age: 22,
+      },
+    ],
+  });
 });
 
 app.listen(8080, () => {
