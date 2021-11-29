76bd5812c527180a7f59468c722383e7b33b4cff
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-28 23:06:06 -0500

    organizing folder structure

---
+++ frontend/src/pages/auth/PageNotFound.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const PageNotFound: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Page Not Found</h1>
+      </header>
+    </div>
+  );
+};
+
+export default PageNotFound;
