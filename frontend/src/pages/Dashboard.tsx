171915a0f4224e2b496436d349f75343f5aa91e4
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-29 02:11:26 -0500

    adding useEffect to call onAuthStateChanged firebase method to check if user is authenticated

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,14 +1,39 @@
 import React from 'react';
 import AssetContainerComponent from '../components/DashboardComponents/AssetContainer/AssetContainerComponent';
+import { onAuthStateChanged } from '@firebase/auth';
+import { auth } from '../config/firebase';
+
+import { useHistory } from 'react-router-dom';
+import DashboardHeader from '../components/DashboardComponents/DashboardHeader/DashboardHeader';
+
 interface Props {}
 
 const Dashboard: React.FC<Props> = () => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState<boolean>(true);
+  const [auth_user, setUser] = React.useState<string | null>('');
+
+  const history = useHistory();
+  React.useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        setIsAuthenticated(false);
+        history.push('/login');
+      } else {
+        setIsAuthenticated(true);
+        setUser(user.email);
+      }
+    });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header" style={{ marginLeft: '80px' }}>
-        <AssetContainerComponent />
-      </header>
-    </div>
+    <>
+      <DashboardHeader />
+      <div className="App">
+        <header className="App-header" style={{ marginLeft: '80px' }}>
+          <AssetContainerComponent />
+        </header>
+      </div>
+    </>
   );
 };
 
