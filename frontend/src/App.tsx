87811a25597393f5020b08c79a65a6294271122a
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-29 02:10:49 -0500

    removing useEffect import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import './App.css';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/auth/Home';
