9c7f373c842919a1d60d0722bc61f35a4c940340
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-29 18:07:59 -0500

    adding firebases decoded user object to req.user object

---
+++ backend/src/middleware/middlewares.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response, NextFunction, Application } from 'express';
+const firebaseAdmin = require('../config/firebase');
+
+class Middleware {
+  constructor() {}
+  async verifyAccessToken(req: Request, res: Response, next: NextFunction) {
+    const accessToken = req.headers['authorization'];
+    if (!accessToken) {
+      return res.status(401).send('Access token is required');
+    }
+    try {
+      const decoded = await firebaseAdmin.auth().verifyIdToken(accessToken);
+      if (decoded) {
+        req.user = decoded;
+        return next();
+      }
+    } catch (error) {
+      return res.status(401).send('Invalid access token');
+    }
+  }
+}
+const middleware = new Middleware();
+export default middleware;
