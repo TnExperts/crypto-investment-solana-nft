36e7e8adb2d1bc9d4cfb6ada6ad8035a729235e3
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-30 04:01:57 -0500

    CardComponent.tsx gets CardComponent props and renders a Card using Material UI, again only used for view

---
+++ frontend/src/components/HomeCardComponent/CardComponent.tsx
@@ -0,0 +1,26 @@
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardComponentProps from '../../interfaces/IHomeCardComponent';
+
+const CardComponent: React.FC<CardComponentProps> = (props) => {
+  // View
+  return (
+    <div className="content">
+      <Card
+        sx={{
+          minWidth: 275,
+          background: `linear-gradient(${props.color1},${props.color2})`,
+          color: 'white',
+          transform: `rotateY(360deg)`,
+          transition: '0.3s',
+        }}
+      >
+        <CardContent>
+          <h4>{props.title}</h4>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
+
+export default CardComponent;
