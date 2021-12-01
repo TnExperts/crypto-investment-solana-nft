bab235f61f31cc11ad65d37398940f09c5c77bd7
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-01 18:44:09 -0500

    adding firestore db url to firebase config

--- frontend/src/config/config.ts
+++ frontend/src/config/config.ts
@@ -2,6 +2,7 @@ const config = {
   firebaseConfig: {
     apiKey: 'AIzaSyAXIBXbIRokQnx6ApEpMlmqK56DyHlSjG0',
     authDomain: 'crypto-tra-491d2.firebaseapp.com',
+    databaseURL: 'https://crypto-tra-491d2-default-rtdb.firebaseio.com',
     projectId: 'crypto-tra-491d2',
     storageBucket: 'crypto-tra-491d2.appspot.com',
     messagingSenderId: '443334768190',
