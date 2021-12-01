15dd670858bf20bd8a0f8ca55d7757b7b9fafeca
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-01 18:43:36 -0500

    removing unnecessary react imports from login and register

--- frontend/src/pages/Auth/Login/LoginModel.ts
+++ frontend/src/pages/Auth/Login/LoginModel.ts
@@ -1,4 +1,3 @@
-import React from 'react';
 import { observable, action, makeAutoObservable } from 'mobx';
 import { RouteComponentProps } from 'react-router-dom';
 import { signInWithEmailAndPassword } from 'firebase/auth';
