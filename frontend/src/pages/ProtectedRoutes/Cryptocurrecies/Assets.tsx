240915536bdb975380c68983ec0bb1f3bc25e814
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-02 16:56:22 -0500

    Assets page renders asset table view with list of crptos

---
+++ frontend/src/pages/ProtectedRoutes/Cryptocurrecies/Assets.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import AssetsViewModel from './AssetsViewModel';
+import AssetsTableView from './AssetsTableView';
+import './Assets.css';
+
+interface Props {}
+
+const Assets: React.FC<Props> = () => {
+  const assetsViewModel = new AssetsViewModel();
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AssetsTableView viewModel={assetsViewModel} />
+      </header>
+    </div>
+  );
+};
+
+export default Assets;
