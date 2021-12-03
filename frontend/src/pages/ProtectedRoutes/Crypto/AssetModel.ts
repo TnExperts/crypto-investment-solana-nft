13a38a5ead7d9ff43bbea08244309c4b2d0c894d
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-03 16:32:51 -0500

    1. Splitting up interface to its own folder 2. Adding fetchAsset function to query details of a currency 3. Adding fetchAssetPriceChart to query market chart dat for charts TODO: next task

--- frontend/src/pages/ProtectedRoutes/Crypto/AssetModel.ts
+++ frontend/src/pages/ProtectedRoutes/Crypto/AssetModel.ts
@@ -1,21 +1,5 @@
-import { observable, action, makeAutoObservable } from 'mobx';
-
-interface IAssetModel {
-  rank: number;
-  id: string;
-  name: string;
-  symbol: string;
-  logo: string;
-  price: string;
-  market_cap: string;
-  percent_change_24h: string;
-  percent_change_7d: string;
-  volume_24h: string;
-  supply: string;
-  link: string;
-  description: string;
-}
-
+import { observable } from 'mobx';
+import { IAssetModel } from './Interface/IAssetModel';
 class AssetModel {
   @observable assetList: IAssetModel[] = [];
   @observable assetName: string = '';
@@ -24,7 +8,23 @@ class AssetModel {
     return fetch(`http://localhost:8080/api/cryptocurrencies/${this.assetName}`)
       .then((res) => res.json())
       .then((data) => {
-        this.assetList.push(data);
+        this.assetList.push({
+          rank: data.market_cap_rank,
+          id: data.id,
+          name: data.name,
+          symbol: data.symbol.toUpperCase(),
+          homepage: data.links.homepage[0],
+          logo: data.image.small,
+          price: data.market_data.current_price.usd.toLocaleString(),
+          all_time_high: data.market_data.ath.usd.toLocaleString(),
+          all_time_low: data.market_data.atl.usd.toLocaleString(),
+          market_cap: data.market_data.market_cap.usd.toLocaleString(),
+          volume_24h: data.market_data.total_volume.usd.toLocaleString(),
+          percent_change_24h:
+            data.market_data.price_change_percentage_24h.toLocaleString(),
+          percent_change_7d:
+            data.market_data.price_change_percentage_7d.toLocaleString(),
+        });
         return this.assetList;
       })
       .catch((err) => {
@@ -32,7 +32,19 @@ class AssetModel {
       });
   };
 
-  fetchAssetPriceChart = (id: string) => {};
+  fetchAssetPriceChart = () => {
+    return fetch(
+      `http://localhost:8080/api/cryptocurrencies/chart/${this.assetName}`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        return this.assetList;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 }
 
 export default AssetModel;
