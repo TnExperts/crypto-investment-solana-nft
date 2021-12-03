c4032c7c460ef64bef52a675329aae9fa556b557
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-03 18:48:31 -0500

    Refactor:
    1. Splitting up Table to clean code
    2. Passing down viewModel props to each Table component for them to render

--- frontend/src/pages/ProtectedRoutes/Crypto/components/AssetMetaData.tsx
+++ frontend/src/pages/ProtectedRoutes/Crypto/components/AssetMetaData.tsx
@@ -1,156 +1,56 @@
 import React from 'react';
-import { IAssetModel } from '../Interface/IAssetModel';
-import { Link } from 'react-router-dom';
 import Table from '@mui/material/Table';
-import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
 import AssetViewModel from '../AssetViewModel';
+import Card from '@mui/material/Card';
+import AssetMetaDataRank from './AssetMetaDataComponents/AssetMetaDataRank';
+import AssetMetaDataName from './AssetMetaDataComponents/AssetMetaDataName';
+import AssetMetaDataSymbol from './AssetMetaDataComponents/AssetMetaDataSymbol';
+import AssetMetaDataCurrentPrice from './AssetMetaDataComponents/AssetMetaDataCurrentPrice';
+import AssetMetaDataChange24 from './AssetMetaDataComponents/AssetMetaDataChange24';
+import AssetMetaDataChange7 from './AssetMetaDataComponents/AssetMetaDataChange7';
+import AssetMetaDataMarketCap from './AssetMetaDataComponents/AssetMetaDataMarketCap';
+import AssetMetaDataVolume from './AssetMetaDataComponents/AssetMetaDataVolume';
+import AssetMetaDataAth from './AssetMetaDataComponents/AssetMetaDataAth';
+import AssetMetaDataAtl from './AssetMetaDataComponents/AssetMetaDataAtl';
+import AssetMetaDataHomePage from './AssetMetaDataComponents/AssetMetaDataHomePage';
 
 interface Props {
-  asset: IAssetModel[];
   viewModel: AssetViewModel;
 }
 
 const AssetMetaData: React.FC<Props> = (props) => {
-  const { asset, viewModel } = props;
-  const [crypto, setCrypto] = React.useState<IAssetModel[]>(asset);
-
-  React.useEffect(() => {
-    setCrypto(asset);
-  }, [asset]);
+  const { viewModel } = props;
   return (
     <div className="asset-stats">
       <TableContainer>
+        <Card
+          sx={{
+            color: 'white',
+            bgcolor: '#121214',
+            borderRadius: '10px',
+          }}
+        >
+          <h4>{viewModel.assetModel.name} Stats</h4>
+        </Card>
         <Table size="small" aria-label="small table">
-          {crypto.map((item) => (
-            <>
-              <caption style={{ color: 'white' }}>
-                <h3>{item.name} Metadata</h3>
-              </caption>
-              <TableHead>
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>Rank</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <h3>{item.rank}</h3>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>Name</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <h3>{item.name}</h3>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>Symbol</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <h3>{item.symbol}</h3>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>Current Price</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <h3>${item.price}</h3>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>24h% Change</h4>
-                  </TableCell>
-                  {viewModel.isPositive(item.percent_change_24h) ? (
-                    <TableCell sx={{ color: 'green' }}>
-                      <h3>{item.percent_change_24h}%</h3>
-                    </TableCell>
-                  ) : (
-                    <TableCell sx={{ color: 'red' }}>
-                      <h3>{item.percent_change_24h}%</h3>
-                    </TableCell>
-                  )}
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>7d% Change</h4>
-                  </TableCell>
-                  {viewModel.isPositive(item.percent_change_7d) ? (
-                    <TableCell sx={{ color: 'green' }}>
-                      <h3>{item.percent_change_7d}%</h3>
-                    </TableCell>
-                  ) : (
-                    <TableCell sx={{ color: 'red' }}>
-                      <h3>{item.percent_change_7d}%</h3>
-                    </TableCell>
-                  )}
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>Market Cap</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <h3>${item.market_cap}</h3>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>Volume (24h)</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <h3>${item.volume_24h}</h3>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>All time high</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <h3>${item.all_time_high}</h3>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>All time low</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <h3>${item.all_time_low}</h3>
-                  </TableCell>
-                </TableRow>
-
-                <TableRow>
-                  <TableCell sx={{ color: 'white' }} variant="head">
-                    <h4>Hompage</h4>
-                  </TableCell>
-                  <TableCell sx={{ color: 'white' }}>
-                    <Link
-                      className="link1"
-                      to={{
-                        pathname: `https://${item.homepage.split('/')[2]}`,
-                      }}
-                      target="_blank"
-                    >
-                      <h3>{item.name} Website</h3>
-                    </Link>
-                  </TableCell>
-                </TableRow>
-              </TableHead>
-            </>
-          ))}
+          <caption style={{ color: 'white' }}>
+            <h3>{viewModel.assetModel.name} Stats</h3>
+          </caption>
+          <TableHead>
+            <AssetMetaDataRank viewModel={viewModel} />
+            <AssetMetaDataName viewModel={viewModel} />
+            <AssetMetaDataSymbol viewModel={viewModel} />
+            <AssetMetaDataCurrentPrice viewModel={viewModel} />
+            <AssetMetaDataChange24 viewModel={viewModel} />
+            <AssetMetaDataChange7 viewModel={viewModel} />
+            <AssetMetaDataMarketCap viewModel={viewModel} />
+            <AssetMetaDataVolume viewModel={viewModel} />
+            <AssetMetaDataAth viewModel={viewModel} />
+            <AssetMetaDataAtl viewModel={viewModel} />
+            <AssetMetaDataHomePage viewModel={viewModel} />
+          </TableHead>
         </Table>
       </TableContainer>
     </div>
