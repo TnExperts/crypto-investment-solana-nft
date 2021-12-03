7455eb07a678493a52dcc91919fad968927c9c9e
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-02 22:03:03 -0500

    adding more asset data such as % change over 24h and 7d, volume, symbol, and logo details

--- frontend/src/pages/ProtectedRoutes/Cryptocurrecies/AssetsModel.ts
+++ frontend/src/pages/ProtectedRoutes/Cryptocurrecies/AssetsModel.ts
@@ -1,12 +1,17 @@
 import { observable, action, makeAutoObservable } from 'mobx';
 
 interface IAssetsModel {
+  rank: number;
   id: string;
   name: string;
   symbol: string;
-  price: number;
-  market_cap: number;
-  percent_change_24h: number;
+  logo: string;
+  price: string;
+  market_cap: string;
+  percent_change_24h: string;
+  percent_change_7d: string;
+  volume_24h: string;
+  supply: string;
 }
 
 class AssetsModel {
@@ -18,12 +23,19 @@ class AssetsModel {
       .then((data) => {
         data.forEach((item: any) => {
           this.assetsList.push({
+            rank: item.market_data.market_cap_rank,
             id: item.id,
             name: item.name,
-            symbol: item.symbol,
-            price: item.market_data.current_price.usd,
-            market_cap: item.market_data.market_cap.usd,
-            percent_change_24h: item.market_data.price_change_percentage_24h,
+            symbol: item.symbol.toUpperCase(),
+            logo: item.image.thumb,
+            price: item.market_data.current_price.usd.toLocaleString(),
+            market_cap: item.market_data.market_cap.usd.toLocaleString(),
+            percent_change_24h:
+              item.market_data.price_change_percentage_24h.toLocaleString(),
+            percent_change_7d:
+              item.market_data.price_change_percentage_7d.toLocaleString(),
+            volume_24h: item.market_data.total_volume.usd.toLocaleString(),
+            supply: item.market_data.circulating_supply.toLocaleString(),
           });
         });
         return this.assetsList;
