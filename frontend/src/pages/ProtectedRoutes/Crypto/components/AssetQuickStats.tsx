92e260980a5b4d6f51afa66372ec56673db632ae
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-03 18:50:24 -0500

    Refactor: 1. Receiving viewModel as a prop 2. Removing looping as it's unnecessary now, just using viewModel to get data

--- frontend/src/pages/ProtectedRoutes/Crypto/components/AssetQuickStats.tsx
+++ frontend/src/pages/ProtectedRoutes/Crypto/components/AssetQuickStats.tsx
@@ -1,35 +1,35 @@
 import React from 'react';
-import { IAssetModel } from '../Interface/IAssetModel';
+import AssetViewModel from '../AssetViewModel';
 import Card from '@mui/material/Card';
 import '../Asset.css';
 
 interface Props {
-  asset: IAssetModel[];
+  viewModel: AssetViewModel;
 }
-
 const AssetQuickStats: React.FC<Props> = (props) => {
-  const { asset } = props;
-  const [crypto, setCrypto] = React.useState<IAssetModel[]>(asset);
-
-  React.useEffect(() => {
-    setCrypto(asset);
-  }, [asset]);
+  const { viewModel } = props;
   return (
     <div className="price-quick-stats">
-      {crypto.map((item) => (
-        <Card sx={{ color: 'white', bgcolor: '#121214', borderRadius: '10px' }}>
-          <span className="wrap-quick-stats">
-            <h4>${item.price}</h4>
-            {item.percent_change_24h.charAt(0) === '-' ? (
-              <h6 className="price-change-24h">{item.percent_change_24h}%</h6>
-            ) : (
-              <h6 className="price-change-24h-green">
-                {item.percent_change_24h}
-              </h6>
-            )}
-          </span>
-        </Card>
-      ))}
+      <Card
+        sx={{
+          color: 'white',
+          bgcolor: '#121214',
+          borderRadius: '10px',
+        }}
+      >
+        <span className="wrap-quick-stats">
+          <h4>${viewModel.assetModel.price}</h4>
+          {viewModel.assetModel.percent_change_24h.charAt(0) === '-' ? (
+            <h6 className="price-change-24h">
+              {viewModel.assetModel.percent_change_24h}%
+            </h6>
+          ) : (
+            <h6 className="price-change-24h-green">
+              {viewModel.assetModel.percent_change_24h}
+            </h6>
+          )}
+        </span>
+      </Card>
     </div>
   );
 };
