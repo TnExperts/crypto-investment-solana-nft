db1cd6b43c3de7dc649777c055ce313a8a881c6f
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-03 20:02:49 -0500

    Refactor: 1. Creating a helper method to parse fetch data

--- frontend/src/pages/ProtectedRoutes/Cryptocurrecies/AssetsModel.ts
+++ frontend/src/pages/ProtectedRoutes/Cryptocurrecies/AssetsModel.ts
@@ -4,25 +4,30 @@ import { IAssetsModel } from './Interface/IAssetsModel';
 class AssetsModel {
   @observable assetsList: IAssetsModel[] = [];
 
+  convertToObject = (data: any): IAssetsModel => {
+    const obj = {
+      rank: data.market_data.market_cap_rank,
+      id: data.id,
+      name: data.name,
+      symbol: data.symbol.toUpperCase(),
+      logo: data.image.thumb,
+      price: data.market_data.current_price.usd.toLocaleString(),
+      market_cap: data.market_data.market_cap.usd.toLocaleString(),
+      percent_change_24h:
+        data.market_data.price_change_percentage_24h.toLocaleString(),
+      percent_change_7d:
+        data.market_data.price_change_percentage_7d.toLocaleString(),
+      volume_24h: data.market_data.total_volume.usd.toLocaleString(),
+    };
+    return obj;
+  };
+
   fetchAssets = () => {
     return fetch('http://localhost:8080/api/cryptocurrencies')
       .then((res) => res.json())
       .then((data) => {
         data.forEach((item: any) => {
-          this.assetsList.push({
-            rank: item.market_data.market_cap_rank,
-            id: item.id,
-            name: item.name,
-            symbol: item.symbol.toUpperCase(),
-            logo: item.image.thumb,
-            price: item.market_data.current_price.usd.toLocaleString(),
-            market_cap: item.market_data.market_cap.usd.toLocaleString(),
-            percent_change_24h:
-              item.market_data.price_change_percentage_24h.toLocaleString(),
-            percent_change_7d:
-              item.market_data.price_change_percentage_7d.toLocaleString(),
-            volume_24h: item.market_data.total_volume.usd.toLocaleString(),
-          });
+          this.assetsList.push(this.convertToObject(item));
         });
         return this.assetsList;
       })
