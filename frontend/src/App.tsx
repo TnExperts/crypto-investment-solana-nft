11856a08529f7ce196406e68b7f29a928bd5cfb6
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-04 09:49:06 -0500

    Adding new routes - Watchlist - Trading - Nft

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,27 +4,30 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './pages/LandingPage/Home';
 import Login from './pages/Auth/Login/Login';
 import Register from './pages/Auth/Register/Register';
-import Dashboard from './pages/Dashboard';
-import Header from './components/Header/HeaderComponent';
 import Assets from './pages/ProtectedRoutes/Cryptocurrecies/Assets';
 import Crypto from './pages/ProtectedRoutes/Crypto/Asset';
+import WatchList from './pages/ProtectedRoutes/Watchlist/WatchList';
+import Trading from './pages/ProtectedRoutes/Trading/Trading';
+import NFT from './pages/ProtectedRoutes/NFT/NFT';
+import FooterComponent from './components/FooterComponent/FooterComponent';
 
 const App: React.FC<{}> = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
 
-        {/* Protected Routes */}
+        {/* TODO: Add Protected Route */}
 
         <Route exact path="/cryptocurrencies/" component={Assets} />
         <Route path="/cryptocurrencies/:id" component={Crypto} />
-        <Route exact path="/dashboard" component={Dashboard} />
-        {/* <Route exact path="/dashboard" component={Dashboard} /> */}
+        <Route exact path="/watchlist" component={WatchList} />
+        <Route exact path="/trade" component={Trading} />
+        <Route exact path="/nft" component={NFT} />
       </Switch>
+      <FooterComponent />
     </BrowserRouter>
   );
 };
