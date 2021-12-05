782ef987fba5e2cf50f4dc975ec36590393f3a52
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-05 12:56:20 -0500

    Four new db methods: 1. watchlist handler checks if the an asset exists in the user watchlist or not 2. if it exists, then the delete method is called 3. if not, then the add method is called

--- backend/src/db/db_operations.ts
+++ backend/src/db/db_operations.ts
@@ -16,11 +16,50 @@ const add_asset_to_watchlist = async (
   user: any,
   to_add_watchlist_data: string
 ) => {
-  console.log(user, to_add_watchlist_data);
-  const add_to_watchlist = db.collection('users').doc(user.uid);
+  const add_to_watchlist = db.collection('users').doc(user);
   await add_to_watchlist.update({
     watchlist: admin.firestore.FieldValue.arrayUnion(to_add_watchlist_data),
   });
 };
 
-module.exports = { add_new_user, add_asset_to_watchlist };
+const delete_asset_from_watchlist = async (
+  user: any,
+  to_delete_watchlist_data: string
+) => {
+  const delete_from_watchlist = db.collection('users').doc(user);
+  await delete_from_watchlist.update({
+    watchlist: admin.firestore.FieldValue.arrayRemove(to_delete_watchlist_data),
+  });
+};
+
+const check_if_asset_is_in_watchlist_db = async (user: any) => {
+  const check_watchlist = db.collection('users').doc(user);
+  const userRef = await check_watchlist.get();
+  if (userRef.exists) {
+    return userRef.data().watchlist;
+  }
+};
+
+const watchlist_handler_db = async (
+  user: any,
+  to_add_watchlist_data: string
+) => {
+  const check_watchlist = db.collection('users').doc(user.uid);
+  const userRef = await check_watchlist.get();
+  if (userRef.exists) {
+    const watchlist = userRef.data().watchlist;
+    if (watchlist.includes(to_add_watchlist_data)) {
+      console.log('Asset already in watchlist. Deleting...');
+      await delete_asset_from_watchlist(user.uid, to_add_watchlist_data);
+    } else {
+      console.log('Asset not in watchlist. Adding...');
+      await add_asset_to_watchlist(user.uid, to_add_watchlist_data);
+    }
+  }
+};
+
+module.exports = {
+  add_new_user,
+  check_if_asset_is_in_watchlist_db,
+  watchlist_handler_db,
+};
