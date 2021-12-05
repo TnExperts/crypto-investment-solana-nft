6343eaed48459907eb711e01d2be685da292280e
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-04 19:21:09 -0500

    Implementing a protected route provider for private routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,9 +7,13 @@ import Register from './pages/Auth/Register/Register';
 import Assets from './pages/ProtectedRoutes/Cryptocurrecies/Assets';
 import Crypto from './pages/ProtectedRoutes/Crypto/Asset';
 import WatchList from './pages/ProtectedRoutes/Watchlist/WatchList';
-import Trading from './pages/ProtectedRoutes/Trading/Trading';
+// import Trading from './pages/ProtectedRoutes/Trading/Trading';
 import NFT from './pages/ProtectedRoutes/NFT/NFT';
 import FooterComponent from './components/FooterComponent/FooterComponent';
+// import { onAuthStateChanged } from '@firebase/auth';
+// import { auth } from './config/firebase';
+// import { useHistory } from 'react-router-dom';
+import ProtectedRoute from './pages/Auth/ProtectedRoute/ProtectedRoute';
 
 const App: React.FC<{}> = () => {
   return (
@@ -18,14 +22,13 @@ const App: React.FC<{}> = () => {
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
-
-        {/* TODO: Add Protected Route */}
-
-        <Route exact path="/cryptocurrencies/" component={Assets} />
-        <Route path="/cryptocurrencies/:id" component={Crypto} />
-        <Route exact path="/watchlist" component={WatchList} />
-        <Route exact path="/trade" component={Trading} />
-        <Route exact path="/nft" component={NFT} />
+        <ProtectedRoute>
+          <Route exact path="/cryptocurrencies/" component={Assets} />
+          <Route path="/cryptocurrencies/:id" component={Crypto} />
+          <Route exact path="/watchlist" component={WatchList} />
+          {/* <Route exact path="/trade" component={Trading} /> */}
+          <Route exact path="/nft" component={NFT} />
+        </ProtectedRoute>
       </Switch>
       <FooterComponent />
     </BrowserRouter>
