15dd78eba0811ffcb5c8ee1ac2396877a5b8682f
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-05 14:13:22 -0500

    uncommenting route

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -5,12 +5,13 @@ const {
   get_crypto,
   get_crypto_market_history,
   watchlist_handler,
+  get_user_watchlist,
 } = require('../controllers/controller');
 
 router.get('/cryptocurrencies', get_cryptocurrencies);
 router.get('/cryptocurrencies/:id', get_crypto);
 router.get('/cryptocurrencies/chart/:id', get_crypto_market_history);
 router.post('/watchlist', watchlist_handler);
-// router.get('/api/watchlist', get_user_watchlist);
+router.get('/watchlist', get_user_watchlist);
 
 module.exports = router;
