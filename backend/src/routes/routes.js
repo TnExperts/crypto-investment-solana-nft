0bae63cec97ace4e9ea777b6f746eb0b6ea56763
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-05 10:53:13 -0500

    adding /watchlist route

--- backend/src/routes/routes.js
+++ backend/src/routes/routes.js
@@ -4,13 +4,14 @@ const {
   get_cryptocurrencies,
   get_crypto,
   get_crypto_market_history,
+  add_to_watchlist,
 } = require('../controllers/controller');
 
 router.get('/cryptocurrencies', get_cryptocurrencies);
 router.get('/cryptocurrencies/:id', get_crypto);
 router.get('/cryptocurrencies/chart/:id', get_crypto_market_history);
+router.post('/watchlist', add_to_watchlist);
 // router.get('/api/watchlist', get_user_watchlist);
-// router.post('/api/watchlist', add_to_watchlist);
 // router.delete('/api/watchlist', remove_from_watchlist);
 
 module.exports = router;
