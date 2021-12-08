2e93a7fc8621d091966bc08499390f4fbea7e7df
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-07 19:10:22 -0500

    removing reload for now, have to find a better method

--- frontend/src/pages/ProtectedRoutes/Cryptocurrecies/components/AssetsTableViewWatchList.tsx
+++ frontend/src/pages/ProtectedRoutes/Cryptocurrecies/components/AssetsTableViewWatchList.tsx
@@ -10,16 +10,11 @@ interface Props {
 
 const AssetsTableViewWatchList: React.FC<Props> = (props) => {
   const { viewModel, name, is_in_watchlist } = props;
-
+  const check_watchlist = async () => {
+    viewModel.check_watchlist(name);
+  };
   return (
-    <TableCell
-      component="th"
-      scope="row"
-      onClick={() => {
-        viewModel.check_watchlist(name);
-        window.location.reload();
-      }}
-    >
+    <TableCell component="th" scope="row" onClick={check_watchlist}>
       {is_in_watchlist ? (
         <DiamondIcon sx={{ color: '#6DD5FA', cursor: 'pointer' }}></DiamondIcon>
       ) : (
