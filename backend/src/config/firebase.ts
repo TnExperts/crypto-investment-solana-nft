ccece0fd3225a412ed11134444a4716959618bea
Tapabrata Dey <tapabratadey02@gmail.com>
2021-12-07 19:05:41 -0500

    updating firebase keys to accept env vars

--- backend/src/config/firebase.ts
+++ backend/src/config/firebase.ts
@@ -3,13 +3,18 @@ const {
   Timestamp,
   FieldValue,
 } = require('firebase-admin/firestore');
-
+require('dotenv').config();
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./firebase-service.json');
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    project_id: process.env.project_id,
+    private_key_id: process.env.private_key_id,
+    private_key: process.env.private_key,
+    client_email: process.env.client_email,
+    client_id: process.env.client_id,
+    client_x509_cert_url: process.env.auth_provider_x509_cert_url,
+  }),
   databaseURL: 'https://crypto-tra-491d2-default-rtdb.firebaseio.com',
 });
 
